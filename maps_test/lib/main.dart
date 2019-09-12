6101ccdf9763981660bea8afc9a67808653e4eb8
Philip Wee <philip_andrew@mymail.sutd.edu.sg>
2019-09-12 16:57:49 +0800

    Made Maps work

--- maps_test/lib/main.dart
+++ maps_test/lib/main.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'dart:convert';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
@@ -11,8 +10,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Google Maps Demo',
-      home: MapSample(),
+      title: "testing one 2 three",
+      home: MapSample()
     );
   }
 }
@@ -24,91 +23,165 @@ class MapSample extends StatefulWidget {
 
 class MapSampleState extends State<MapSample> {
   Completer<GoogleMapController> _controller = Completer();
+  List<Polyline> polylines = [];
+
 
   //Create the get request function
-  static Future<Map<String, dynamic>> _getCalculate() async {
+  Future<Map<String, dynamic>> _getCalculate() async {
     final result =
-        await http.get('http://192.168.194.178:5000/session/123456/calculate');
+        await http.get('http://192.168.194.210:5000/session/123456/results');
     if (result.statusCode == 200) {
       Map<String, dynamic> results = jsonDecode(result.body);
       print(results);
       print(results['possible_locations']);
       return results;
     } else {
-      throw ("Error getting results");
+      throw ("Error getting results with statusCode " + result.statusCode.toString());
     }
   }
 
-  var results;
-
-  final Polyline _testline = Polyline(
-      polylineId: PolylineId('testline'),
-      points: [
-        LatLng(37.42796133580664, -122.085749655962),
-        LatLng(37.43296265331129, -122.08832357078792)
-      ],
-      visible: true);
-
-  static final CameraPosition _kGooglePlex = CameraPosition(
-    target: LatLng(37.42796133580664, -122.085749655962),
-    zoom: 14.4746,
+  static final CameraPosition _kSingapore = CameraPosition(
+    target: LatLng(1.3385253,103.8),
+    zoom: 10,
   );
+  
 
-  static final CameraPosition _kLake = CameraPosition(
-      bearing: 192.8334901395799,
-      target: LatLng(37.43296265331129, -122.08832357078792),
-      tilt: 59.440717697143555,
-      zoom: 19.151926040649414);
 
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          Center(
-              child: SizedBox(
-                  height: 500.0,
-                  child: GoogleMap(
-                    mapType: MapType.hybrid,
-                    initialCameraPosition: _kGooglePlex,
-                    onMapCreated: (GoogleMapController controller) {
-                      _controller.complete(controller);
-                    },
-                    polylines: Set<Polyline>.of([_testline]),
-                  ))),
-          Center(
-            child: ListView.builder(
-              //  _getCalculate().then((data) {
-              //     results = data;
-              //   }),
-              itemCount: results['possible_locations'].keys.length,
-              itemBuilder: (context, index) {
-                return Text(results['possible_locations'][index.toString()]);
-              }
-            )
-            
-          )    
-        ]);
-    return new Scaffold(
-      body: GoogleMap(
-        mapType: MapType.hybrid,
-        initialCameraPosition: _kGooglePlex,
-        onMapCreated: (GoogleMapController controller) {
-          _controller.complete(controller);
-        },
-        polylines: Set<Polyline>.of([_testline]),
-      ),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: _getCalculate,
-        label: Text('To the lake!'),
-        icon: Icon(Icons.directions_boat),
-      ),
+  //Build the map
+  GoogleMap displayMap() {
+    return GoogleMap(
+      mapType: MapType.normal,
+      initialCameraPosition: _kSingapore,
+      onMapCreated: (GoogleMapController controller) {
+        _controller.complete(controller);
+      },
+      polylines: Set<Polyline>.of(polylines),
     );
   }
 
-  Future<void> _goToTheLake() async {
-    final GoogleMapController controller = await _controller.future;
-    controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
+  //Make a list for the different colors
+  List<Color> colors = <Color>[
+    Colors.blue,
+    Colors.purple,
+    Colors.red,
+    Colors.green,
+    Colors.pink
+  ];
+
+  //Create a polyline from array
+  Polyline _makeLine(List<dynamic> latitude, List<dynamic> longtitude, int polylineid) {
+    List<LatLng> latLngList = [];
+    for (var i = 0; i < latitude.length; i++) {
+      print(latitude[i]);
+      print(longtitude[i]);
+      latLngList.add(LatLng(latitude[i],longtitude[i]));
+    }
+    return Polyline(
+      polylineId: PolylineId(polylineid.toString()),
+      color: colors[polylineid%5],
+      width: 5,
+      points: latLngList
+    );
   }
+
+  //Drawing the map lines
+  Future<void> _drawRoutes(String locationName, Map<String,dynamic> data) async {
+    print('Now showing routes for ' + locationName);
+    print(data[locationName]);
+    List<Polyline> polylineContainer = [];
+    int lineIterator = 0;
+    LatLng destinationLatLng;
+    for (String key in data[locationName].keys) {
+        var latitude = data[locationName][key]['latitude'];
+        var longtitude = data[locationName][key]['longtitude'];
+        print(latitude);
+        print(longtitude);
+        polylineContainer.add(_makeLine(latitude,longtitude,lineIterator));
+        lineIterator++;
+        if (latitude.length > 0) {
+          destinationLatLng = LatLng(latitude.last,longtitude.last);
+        }
+      }
+    //Draw routes
+    setState(() {
+      polylines = polylineContainer;
+    });
+
+    //Center the map on the appropriate location
+    final CameraPosition _endDestination = CameraPosition(
+      bearing: 0,
+      target: destinationLatLng,
+      tilt: 0,
+      zoom: 14);
+
+
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(CameraUpdate.newCameraPosition(_endDestination));
+
+  }
+
+  //Build the listview
+  FutureBuilder<Map<String,dynamic>> displayPossibleOptions() {
+    //Make a helper function for each button of the listview
+    Card _tile(String locationName, Map<String,dynamic> data) => Card(
+      child: FlatButton(
+        padding: EdgeInsets.all(0.1),
+        onPressed: (){
+          //Place the functionality for showing the route here
+          _drawRoutes(locationName, data);
+        },
+        child: ListTile(
+          title: Text(locationName.toString())
+        )
+      )
+     
+    );
+
+    return FutureBuilder<Map<String,dynamic>>(
+        future: _getCalculate(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            final List<dynamic> possibleLocations = snapshot.data['possible_locations'];
+            return ListView.builder(
+              itemCount: possibleLocations.length,
+              itemBuilder: (BuildContext context, int index) {
+                print(possibleLocations);
+                //Need to add in code to prevent nulls form appearing
+                return _tile(possibleLocations[index], snapshot.data); 
+              },
+            );
+          } else if (snapshot.hasError) {
+            return Text("Error");
+          } else {
+            return CircularProgressIndicator();
+          }
+        }
+      );
+  }
+
+  //Build the main widget
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+        body: SafeArea(
+          child: Center( 
+            child: Container(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Expanded(
+                    child:displayMap(),
+                    flex:5
+                  ),
+                  Expanded(
+                    child:displayPossibleOptions(),
+                    flex:1
+                  )
+                ]
+                )
+            )
+          )
+        )
+      );
+    }
 }
