b5b571d43e3831c400036e7029e971e21a18da45
Stephen Alvin <stephenalvin0912@gmail.com>
2019-09-14 15:35:55 +0800

    First workable mobile app Meetup Mouse ver0.0.1
    Constraints:
    1. Code for server must be running on a desktop
    2. Phone must be online on Zero Tier
    3. Server currently sends a fixed, sessionID and data.
    4. Supporting pages such as, "Contact Us" and "Settings" are incomplete.
    
    Posisble Next Steps:
    1. Create supporting pages
    2. Create Account Management
    3. Make one time user input savable so it can be used again
    4. Mobile App account management

--- Andriod App/lib/main.dart
+++ Andriod App/lib/main.dart
@@ -7,6 +7,7 @@ import 'dart:convert';
 import 'package:esys_flutter_share/esys_flutter_share.dart';
 import 'package:location/location.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 void main() => runApp(MyApp());
 
@@ -784,7 +785,7 @@ class ShareLinkState extends State<ShareLinkWidget> {
                   color: Colors.amber,
                   textColor: Colors.black,
                   onPressed: () {
-                    //TODO go to next page
+                    Navigator.push(context,MaterialPageRoute(builder: (context) => MapSample(data:data)),);
                     print(data.link);
                   },
                   child: Text("Create Meetup!"),
@@ -826,111 +827,267 @@ class ShareLinkState extends State<ShareLinkWidget> {
 
 }
 
-class PickYourPlace extends StatelessWidget {
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Pick Your Place"),
-        backgroundColor: Colors.black,
-      ),
-      body: Center(
-        child: ListView(
-          children:[
-            FlatButton(
-              child: Text('*Changi City Point*'),
-              onPressed: () {Navigator.push(context,MaterialPageRoute(builder: (context) => MapLayout()),);},
-            ),
-            FlatButton(
-              child: Text('*Tampines Hub*'),
-              onPressed: () {Navigator.push(context,MaterialPageRoute(builder: (context) => MapLayout()),);},
-            ),
-            FlatButton(
-              child: Text('*Tampines Mall*'),
-              onPressed: () {Navigator.push(context,MaterialPageRoute(builder: (context) => MapLayout()),);},
-            ),
-            FlatButton(
-              child: Text('*Tampines One*'),
-              onPressed: () {Navigator.push(context,MaterialPageRoute(builder: (context) => MapLayout()),);},
-            ),
-            FlatButton(
-              child: Text('*Tampines Eastpoint*'),
-              onPressed: () {Navigator.push(context,MaterialPageRoute(builder: (context) => MapLayout()),);},
-            ),
-            FlatButton(
-              child: Text('*Tampines Regional Library*'),
-              onPressed: () {Navigator.push(context,MaterialPageRoute(builder: (context) => MapLayout()),);},
-            ),
-            FlatButton(
-              child: Text('*White Sands*'),
-              onPressed: () {Navigator.push(context,MaterialPageRoute(builder: (context) => MapLayout()),);},
-            ),
-            FlatButton(
-              child: Text('*Downtown East*'),
-              onPressed: () {Navigator.push(context,MaterialPageRoute(builder: (context) => MapLayout()),);},
-            ),
-          ],
-        )
-        ),
-      );
-  }
-}
-
-class MapLayout extends StatelessWidget {
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("*Tampines Mall*"),
-        backgroundColor: Colors.black,
-      ),
-      body: MapSample()
-      );
-  }
-}
 
 class MapSample extends StatefulWidget {
+  final PrefData data;
+  MapSample({this.data});
   @override
-  State<MapSample> createState() => MapSampleState();
+  State<MapSample> createState() => MapSampleState(data:data);
 }
 
 class MapSampleState extends State<MapSample> {
+  final PrefData data;
+  MapSampleState({this.data});
   Completer<GoogleMapController> _controller = Completer();
+  List<Polyline> polylines = [];
+  List<Marker> markers = [];
 
-  static final CameraPosition _kGooglePlex = CameraPosition(
-    target: LatLng(1.366960, 103.869424),
-    zoom: 14.4746,
+
+  //Create the get request function
+  Future<Map<String, dynamic>> _getCalculate() async {
+    String lunk = data.link;
+    final result = await http.get('$lunk/calculate');
+    if (result.statusCode == 200) {
+      Map<String, dynamic> results = jsonDecode(result.body);
+      print(results);
+      print(results['possible_locations']);
+      return results;
+    } else {
+      throw ("Error getting results with statusCode " + result.statusCode.toString());
+    }
+  }
+
+  static final CameraPosition _kSingapore = CameraPosition(
+    target: LatLng(1.3385253,103.8),
+    zoom: 10,
   );
+  
 
-  static final CameraPosition _location = CameraPosition(
-      bearing: 0,
-      target: LatLng(1.366960, 103.869424),
-      tilt: 59.440717697143555,
-      zoom: 19.151926040649414);
 
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      body: GoogleMap(
-        mapType: MapType.hybrid,
-        initialCameraPosition: _kGooglePlex,
-        onMapCreated: (GoogleMapController controller) {
-          _controller.complete(controller);
-        },
-      ),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: _goToPosition,
-        label: Text('Lets\'s Go!'),
-        icon: Icon(Icons.fastfood),
-      ),
+  //Build the map
+  GoogleMap displayMap() {
+    return GoogleMap(
+      mapType: MapType.normal,
+      initialCameraPosition: _kSingapore,
+      onMapCreated: (GoogleMapController controller) {
+        _controller.complete(controller);
+      },
+      polylines: Set<Polyline>.of(polylines),
+      markers: Set<Marker>.of(markers),
     );
   }
 
-  Future<void> _goToPosition() async {
-    final GoogleMapController controller = await _controller.future;
-    controller.animateCamera(CameraUpdate.newCameraPosition(_location));
-  }
-}
+  //Make a list for the different colors
+  List<Color> colors = <Color>[
+    Colors.blue,
+    Colors.purple,
+    Colors.red,
+    Colors.green,
+    Colors.pink
+  ];
 
+  //Create a polyline from array
+  Polyline _makeLine(List<dynamic> latitude, List<dynamic> longtitude, int polylineid) {
+    List<LatLng> latLngList = [];
+    for (var i = 0; i < latitude.length; i++) {
+      // print(latitude[i]);
+      // print(longtitude[i]);
+      latLngList.add(LatLng(latitude[i],longtitude[i]));
+    }
+    return Polyline(
+      polylineId: PolylineId(polylineid.toString()),
+      color: colors[polylineid%5],
+      width: 5,
+      points: latLngList
+    );
+  }
+
+  //Make a function that makes a widget appear to show the current selection
+
+
+  //Place a marker
+  Marker _makeMarker(LatLng location, int markerid) {
+    return Marker(
+      markerId: MarkerId(markerid.toString()),
+      position: location,
+      consumeTapEvents: false,
+    );
+  }
+
+  //Drawing the map lines
+  Future<void> _drawRoutes(String locationName, Map<String,dynamic> data) async {
+    print('Now showing routes for ' + locationName);
+    print(data[locationName]);
+    List<Polyline> polylineContainer = [];
+    List<Marker> markerContainer = [];
+    int lineIterator = 0;
+    double destinationLat = 0;
+    double destinationLong = 0;
+    LatLng destinationLatLng;
+    for (String key in data[locationName].keys) {
+        var latitude = data[locationName][key]['latitude'];
+        var longtitude = data[locationName][key]['longtitude'];
+        // print(latitude);
+        // print(longtitude);
+        polylineContainer.add(_makeLine(latitude,longtitude,lineIterator));
+        lineIterator++;
+        
+        destinationLong = data[locationName][key]['restaurant_x'];
+        destinationLat = data[locationName][key]['restaurant_y'];
+        
+        destinationLatLng = LatLng(destinationLat,destinationLong);
+        
+        print(destinationLatLng);
+        markerContainer.add(_makeMarker(destinationLatLng, lineIterator));
+
+      }
+    //Draw routes
+    setState(() {
+      print("111111111111111111111111111111");
+      print(markerContainer.length.toString());
+      _locationName = locationName;
+      _destinationLat = destinationLat;
+      _destinationLong = destinationLong;
+
+      visibility = true;
+      polylines = polylineContainer;
+      markers = markerContainer;
+    });
+
+    //Center the map on the appropriate location
+    final CameraPosition _endDestination = CameraPosition(
+      bearing: 0,
+      target: destinationLatLng,
+      tilt: 0,
+      zoom: 14);
+
+
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(CameraUpdate.newCameraPosition(_endDestination));
+
+  }
+
+  //Build the listview
+  FutureBuilder<Map<String,dynamic>> displayPossibleOptions() {
+    //Make a helper function for each button of the listview
+    Card _tile(String locationName, Map<String,dynamic> data) => Card(
+      child: FlatButton(
+        padding: EdgeInsets.all(0.1),
+        onPressed: (){
+          //Place the functionality for showing the route here
+          _drawRoutes(locationName, data);
+        },
+        child: ListTile(
+          title: Text(locationName.toString())
+        )
+      )
+     
+    );
+
+    return FutureBuilder<Map<String,dynamic>>(
+        future: _getCalculate(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            final List<dynamic> possibleLocations = snapshot.data['possible_locations'];
+            return ListView.builder(
+              itemCount: possibleLocations.length,
+              itemBuilder: (BuildContext context, int index) {
+                print(possibleLocations);
+                //Need to add in code to prevent nulls form appearing
+                return _tile(possibleLocations[index], snapshot.data); 
+              },
+            );
+          } else if (snapshot.hasError) {
+            return Text("Error");
+          } else {
+            return CircularProgressIndicator();
+          }
+        }
+      );
+  }
+
+  //generate the necessary variables for display location details
+  String _locationName = "";
+  bool visibility = false;
+  double _destinationLong = 0;
+  double _destinationLat = 0;
+
+  //Make a button that launches the google maps
+  _launchGoogleMaps() async {
+    String url = "https://www.google.com/maps/dir/?api=1&destination="+_destinationLat.toString()+","+_destinationLong.toString();
+    if (await canLaunch(url)) {
+    await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
+  //Make a widget to display the final location, as well as allow the user to go to google maps
+  Visibility displayLocationDetails() {
+    return Visibility(
+      child: Card(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: 
+          <Widget>[
+            ListTile(
+              title: Center(
+                child:Text(_locationName, style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold))
+              )
+            ),
+            ButtonTheme.bar(
+              child: ButtonBar(
+                children: <Widget>[
+                  FlatButton(
+                    child: Text("SHOW IN MAPS"),
+                    onPressed: () {
+                      _launchGoogleMaps();
+                    },
+                  )
+                ],
+              )
+            )
+          ],
+        )
+      ),
+      visible: visibility,
+    );
+    
+  }
+
+  //Make a widget for the background objects, namely the map and the options
+  Center displayBackground() {
+    return Center( 
+            child: Container(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Expanded(
+                    child:displayMap(),
+                    flex:5
+                  ),
+                  Expanded(
+                    child:displayPossibleOptions(),
+                    flex:1
+                  )
+                ]
+                )
+            )
+          );
+  }
+
+  //Build the main widget
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+        body: SafeArea(
+          child: Stack(
+            children: <Widget>[
+              displayBackground(),
+              displayLocationDetails()
+            ],
+          )
+        )
+      );
+    }
+}
