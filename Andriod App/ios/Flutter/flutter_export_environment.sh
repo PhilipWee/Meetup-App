5c9c3b34eb5be1b7c330c5156f3ad072e6fc68f5
Stephen Alvin <stephenalvin0912@gmail.com>
2020-05-06 12:15:16 +0800

    Update flutter_export_environment.sh

--- Andriod App/ios/Flutter/flutter_export_environment.sh
+++ Andriod App/ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=D:\Flutter\flutter"
-export "FLUTTER_APPLICATION_PATH=D:\Documents\UROP WITH FRIENDS\Meetup App\Andriod App"
+export "FLUTTER_ROOT=C:\Users\steph\Documents\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\steph\Documents\GitHub\Meetup-App\Andriod App"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build\ios"
-export "FLUTTER_FRAMEWORK_DIR=D:\Flutter\flutter\bin\cache\artifacts\engine\ios"
+export "FLUTTER_FRAMEWORK_DIR=C:\Users\steph\Documents\flutter\bin\cache\artifacts\engine\ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
