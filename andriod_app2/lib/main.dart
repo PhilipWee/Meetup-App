280d54337c20240e2e4f0dfa9a5385a325f4f8ac
Stephen Alvin <stephenalvin0912@gmail.com>
2020-05-07 16:18:39 +0800

    Adding files and code... 50%

--- andriod_app2/lib/main.dart
+++ andriod_app2/lib/main.dart
@@ -1,111 +1,397 @@
+import 'dart:convert';
+import 'GoogleSignIn.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:location/location.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:connectivity/connectivity.dart';
+import 'package:http/http.dart' as http;
+
+String globalurl(){
+//   String serverAddress = "http://192.168.194.178:5000";
+//   String serverAddress = "http://169.254.158.154:5000";
+//  String serverAddress = "http://ec2-3-16-181-51.us-east-2.compute.amazonaws.com:5000";
+  String serverAddress = "http://ec2-3-14-68-232.us-east-2.compute.amazonaws.com:5000";
+  return serverAddress;
+}
 
 void main() => runApp(MyApp());
 
+
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      theme: ThemeData(
+          primaryColor: Color(0xffff6347),
+          fontFamily: 'Quicksand'
+      ),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => GoogleSignInPage(),
+      },
+    );
+  }
+}
+class GoogleSignInPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Login',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: LoginPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class CheckNetworkPage extends StatelessWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: StreamBuilder(
+            stream: Connectivity().onConnectivityChanged,
+            builder: (BuildContext ctxt,
+                AsyncSnapshot<ConnectivityResult> snapShot) {
+              if (!snapShot.hasData) return CircularProgressIndicator();
+              var result = snapShot.data;
+              switch (result) {
+                case ConnectivityResult.none:
+                  print("Internet servie unavailable");
+                  return Center(child: Text("No Internet Connection!"));
+                case ConnectivityResult.mobile:
+                case ConnectivityResult.wifi:
+                  return HomeScreen();
+                default:
+                  return Center(child: Text("No Internet Connection!"));
+              }
+            })
+    );
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class HomeScreen extends StatelessWidget {
+  static final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
+      key: _scaffoldKey,
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Home', style: TextStyle(fontWeight: FontWeight.bold),),
+//        backgroundColor: Colors.black,
+        leading: IconButton(
+          icon: Icon(Icons.list),
+          onPressed: (){
+            _scaffoldKey.currentState.openDrawer();
+          },
+        ),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+      body: HomeUsernameWidget(),
+      drawer: Drawer(
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+            DrawerHeader(
+              child: Text('Meetup Mouse',style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 23
+              ),
+              ),
+              decoration: BoxDecoration(
+                shape: BoxShape.rectangle,
+                color: Colors.black,
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
+            ListTile(
+              title: Text('Account'),
+              onTap: () {
+                //TODO
+              },
+            ),
+            ListTile(
+              title: Text('Settings'),
+              onTap: () {
+                // TODO
+              },
+            ),
+            ListTile(
+              title: Text('Contact Us'),
+              onTap: () {
+                //TODO
+              },
+            ),
+            ListTile(
+              title: Text('Exit'),
+              onTap: () {
+                //TODO
+              },
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+
+}
+
+class HomeUsernameWidget extends StatefulWidget {
+  @override
+  HomeUsernameState createState() => HomeUsernameState();
+}
+
+class HomeUsernameState extends State<HomeUsernameWidget> {
+  bool invalidLink = false;
+  static String name;
+  static String sessionlink;
+  final data = PrefData(username:"",activityType: "",lat: 0,long: 0,link:"",transportMode: "",speed: 0, quality: 0,sessionid: '');
+  final textController  = TextEditingController();
+  final idTextController = TextEditingController();
+  final formKey = GlobalKey<FormState>();
+
+  saveMyLocation() async{
+    var location = Location();
+    LocationData currentLocation = await location.getLocation();
+    data.lat = currentLocation.latitude;
+    data.long = currentLocation.longitude;
+    print("User Location Data");
+    print("${data.lat},${data.long}");
+    Map<String,double> mycoordinates = {"mylat":data.lat, "mylong":data.long};
+    List<Placemark> myplacemark = await Geolocator().placemarkFromCoordinates(data.lat,data.long);
+    Placemark placeMark = myplacemark[0];
+    String name = myplacemark[0].thoroughfare.toString();
+    String locality = placeMark.locality;
+    data.userplace = "${name}, ${locality}";
+    print(data.userplace);
+  }
+
+  @override
+  void dispose() {
+    textController.dispose();
+    idTextController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    //Logo and start messages
+    Widget welcomeSection = Container(
+      margin: const EdgeInsets.only(left: 50.0, top: 20.0, right: 50.0),
+      alignment: Alignment.center,
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left:20.0, right: 20.0, top:20.0, bottom: 20.0),
+            child: Image.asset(
+              'images/Mouse_copy.png',
+              scale: 3,
+            ),
+          ),
+          Text(
+            "Start Your Meetup!",
+            style: TextStyle(
+                fontSize: 24.0,
+                fontWeight: FontWeight.w600,
+                fontFamily: 'Pacifico'
+            ),
+          )
+        ],
+      ),
+    );
+
+    //Button Section
+    Widget buttonSection =  Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.only(left: 33.0, right: 33.0),
+          child: Row(
+            children: <Widget>[
+              Expanded(
+                child: FlatButton(
+                  onPressed: () {
+                    saveMyLocation();
+                    if (textController.text != "") {
+                      name = textController.text;
+                      data.username = name;
+                      Navigator.push(context,MaterialPageRoute(builder: (context) => MeetingType(data : data)),);
+//                      dispose();
+                    } else {
+                      name = "Host";
+                      data.username = name;
+                      Navigator.push(context,MaterialPageRoute(builder: (context) => MeetingType(data : data)),);
+//                      Scaffold.of(context).showSnackBar(
+//                          SnackBar(
+//                            content: Text("Please enter your username!"),
+//                            duration: Duration(seconds: 2),
+//                          ));
+                    }},
+                  child: _buildButtonColumn(Colors.black, Icons.arrow_right, 'Get Started!'),
+                  color: Colors.amber,
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(28)),
+                ),
+              )
+            ],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(left: 33.0, right: 33.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Expanded(
+                child: FlatButton(
+                  onPressed: () {
+                    showDialog(
+                        context: context,
+                        barrierDismissible: true,
+                        builder: (BuildContext context) {
+                          if (textController.text != "") {
+                            name = textController.text;
+                            return _buildEnterID(name);
+                          } else {
+                            name = "Anonymouse";
+                            return _buildEnterID(name);
+                          }
+                        }
+                    );
+                  },
+                  child: _buildButtonColumn(Colors.black, Icons.add, 'Join Meetup'),
+                  color: Colors.orange,
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(28)),
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.only(left: 3, right: 3),
+                child: Text(""),
+              ),
+              Expanded(
+                  child: FlatButton(
+                    onPressed: () {
+                      if (textController.text != "") {
+                        Navigator.push(context,MaterialPageRoute(builder: (context) => ShareLinkPage(data : data)),);
+//                      dispose();
+                      } else {
+                        Scaffold.of(context).showSnackBar(
+                            SnackBar(
+                              content: Text("There is no ongoing session!"),
+                              duration: Duration(seconds: 2),
+                            ));
+                      }},
+                    child: _buildButtonColumn(Colors.black, Icons.autorenew, 'My Meetup'),
+                    color: Colors.lightBlue,
+                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(28)),
+                  )
+              )
+            ],
+          ),
+        ),
+      ],
+    );
+
+
+    return Center(
+        child: ListView(
+          children:[
+            welcomeSection,
+            Padding(
+              padding: const EdgeInsets.only(left: 50.0, top: 20.0, right: 50.0, bottom: 50.0),
+              child: TextFormField(
+                controller: textController,
+                decoration: InputDecoration(
+                    hintText: "Name (Optional)"
+                ),
+              ),
+            ),
+            buttonSection,
+          ],
+        )
+    );
+  }
+
+  //Helper method to create button icons with text
+  Row _buildButtonColumn(Color color, IconData icon, String label) {
+    return Row(
+//      mainAxisSize: MainAxisSize.max,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 14.9,
+              fontWeight: FontWeight.bold,
+              color: color,
+            ),
+          ),
+        ),
+        Icon(icon, color: color),
+      ],
+    );
+  }
+
+  //Helper method to build AlertDialog for entering Session ID
+  AlertDialog _buildEnterID(String name) {
+    return AlertDialog(
+      title: Text("Paste Link"),
+      content: Row(
+        children: <Widget>[
+          Expanded(
+              child: Form(
+                key: formKey,
+                child: TextFormField(
+                  controller: idTextController,
+                  autofocus: true,
+                  decoration: InputDecoration(
+                      labelText: "Session Link"
+                  ),
+                  onChanged: (value) {
+                    sessionlink = value;
+                  },
+                  validator: (value) {
+                    if (value.isEmpty || invalidLink){
+                      return "Invalid Link";
+                    }
+                    return null;
+                  },
+                ),
+              )
+          ),
+        ],
+      ),
+      actions: <Widget>[
+        FlatButton(
+          child: Text("Join!"),
+          onPressed: () async {
+            String checkingLink = sessionlink.replaceAll(new RegExp(r'/get_details'), '');
+            print(checkingLink);
+            http.Response response = await http.get(checkingLink);
+            await Future.delayed(Duration(milliseconds: 400));
+            String body = response.body;
+            Map<String, dynamic> DatajsonVersion = jsonDecode(body); //parse the data from server into a map<string,dynamic>
+
+            if (DatajsonVersion.containsKey("error")) {invalidLink = true;}
+
+            if (formKey.currentState.validate() && !invalidLink) {
+
+              data.username = name;
+              data.link = sessionlink;
+              data.sessionid = data.linkParser(sessionlink);
+              print("TEST: ${data.dataMap}");
+
+              Navigator.push(context,MaterialPageRoute(builder: (context) => CustomizationPage(data : data)),);
+
+            } else {
+              print("Validate: ${formKey.currentState.validate()}");
+            }
+          },
+        )
+      ],
+    );
+  }
+
+
 }
