531a11f698815adb92f1c863cc1eb632bc2c8bf2
Veda Alexandra <vedaalexandra@Vedas-MacBook-Pro.local>
2020-05-13 13:55:12 +0800

    working copy buttonn

--- API/run_api_v3.py
+++ API/run_api_v3.py
@@ -155,6 +155,11 @@ def login():
     if request.method == "GET":
         return render_template('newpage.html')
 
+@app.route('/thirdpage/')
+def thirdpage():
+    if request.method == "GET":
+        return render_template('thirdpage.html')
+
 @app.route('/session/<session_id>/results_display')
 def results_display(session_id):
     checkHost = request.args['isHost']
@@ -462,9 +467,9 @@ if __name__ == '__main__':
         
         # Use the application default credentials
         # Use a service account
-        # cred = credentials.Certificate('/Users/vedaalexandra/Desktop/meetup-mouse-265200-2bcf88fc79cc.json')
+        cred = credentials.Certificate('/Users/vedaalexandra/Desktop/meetup-mouse-265200-2bcf88fc79cc.json')
         # cred = credentials.Certificate('C:/Users/Omnif/Documents/meetup-mouse-265200-2bcf88fc79cc.json')
-        cred = credentials.Certificate('/home/ubuntu/Meetup App Confidential/meetup-mouse-265200-2bcf88fc79cc.json')
+        #cred = credentials.Certificate('/home/ubuntu/Meetup App Confidential/meetup-mouse-265200-2bcf88fc79cc.json')
         firebase_admin.initialize_app(cred)
         db = firestore.client()
     else:
