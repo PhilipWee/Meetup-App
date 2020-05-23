96a4e3467a1c9091c1f45c14809406432f1436ab
daviddabid <fan.david@dhs.sg>
2020-05-23 12:06:27 +0800

    Button changes

--- API/run_api_v4.py
+++ API/run_api_v4.py
@@ -36,12 +36,9 @@ if (not len(firebase_admin._apps)):
     # cred = credentials.Certificate('/Users/vedaalexandra/Desktop/meetup-mouse-265200-2bcf88fc79cc.json')
     # cred = credentials.Certificate('C:/Users/Omnif/Documents/meetup-mouse-265200-2bcf88fc79cc.json')
     # cred = credentials.Certificate('/home/ubuntu/Meetup App Confidential/meetup-mouse-265200-2bcf88fc79cc.json')
-<<<<<<< HEAD
     # cred = credentials.Certificate('C:/Users/Philip Wee/Documents/MeetupAppConfidential/meetup-mouse-265200-2bcf88fc79cc.json')
     cred = credentials.Certificate('C:/Users/fanda/Documents/SUTD SOAR/Meetup Mouse/meetup-mouse-265200-2bcf88fc79cc.json')
-=======
-    cred = credentials.Certificate('C:/Users/Philip Wee/Documents/MeetupAppConfidential/meetup-mouse-265200-2bcf88fc79cc.json')
->>>>>>> 3fbf0cb73acfcf3006014efe81fef2c76d792b06
+
     firebase_admin.initialize_app(cred)
     db = firestore.client()
 else:
@@ -188,7 +185,7 @@ Use case: Will be emitted when all there is a matching location
 
 
 Client Emitted Events:
-    
+
 -> Event: 'leave'
 Sample Data: {'room' : <session_id> }
 Use case: Will be triggered when the leave room function is called
@@ -413,7 +410,7 @@ def get_user_sessions():
 #Room joining function
 @socketio.on('join')
 def on_join(data):
-    
+
 
     #Verify the data is in the correct format
     schema_str = """
@@ -430,10 +427,10 @@ def on_join(data):
     join_room(room)
     emit('join_ack',{'message':'Someone has joined the room',
                      'room':room},room=room)
-    
+
 @socketio.on('leave')
 def on_leave(data):
-    
+
 
     #Verify the data is in the correct format
     schema_str = """
@@ -450,7 +447,7 @@ def on_leave(data):
     leave_room(room)
     emit('join_ack',{'message':'Someone has left the room',
                      'room':room},room=room)
-    
+
 @socketio.on('calculation_done')
 def test(data):
     print("Session ID [ " + data['session_id'] + " ] has been calculated")
@@ -503,7 +500,6 @@ def on_swipe_details(data):
         swipe_details = [{userIdentifier:selection}]
 
     doc_ref.update({'swipe_details':swipe_details})
-
 
 
 def check_calculate_done(session_id):
@@ -576,7 +572,7 @@ def update_session_status(session_id,status):
     data = doc_ref.get().get('info')
     data['session_status'] = status
     doc_ref.update({'info':data})
-    
+
 
 def create_firebase_session(content):
     meetup_name = content.pop('meetup_name')
